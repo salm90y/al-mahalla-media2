3fafad9e816c32dc55845285828aa9a0e79e56aa
salm90y <108215437+salm90y@users.noreply.github.com>
2026-09-19 08:32:55 +0300

    feat(ui): add LazyListState to GamesRoomScreen

--- android/app/src/main/java/com/ps1/netplay/ui/compose/GamesRoomScreen.kt
+++ android/app/src/main/java/com/ps1/netplay/ui/compose/GamesRoomScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
